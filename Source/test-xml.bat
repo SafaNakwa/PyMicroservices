c190b150d08a6c3f21eb86d1a64a48c356524bbe
SafaNakwa <49240010+SafaNakwa@users.noreply.github.com>
2019-04-08 19:17:09 +0530

    Update test-xml.bat

--- Source/test-xml.bat
+++ Source/test-xml.bat
@@ -1,7 +1,7 @@
 SET PATH=%PATH%;C:\Python27;C:\Python27\Scripts;C:\Program Files\Git\bin
 del C:\Users\HP\Desktop\PyMicroservices\Source\python_tests_xml\*.*
 del C:\Users\HP\Desktop\PyMicroservices\Source\coverage.xml
-cd c:\skillvelocity\courses\devops\PyMicroservices\Source
+cd C:\Users\HP\Desktop\PyMicroservices\Source
 REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python runtests.py
 REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python -m coverage run -m pytest && coverage xml -o coverage.xml
 REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python -m coverage html -d coverage
