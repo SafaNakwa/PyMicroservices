b4d1f6e659b582fdfafa764e873078be3c714218
SafaNakwa <49240010+SafaNakwa@users.noreply.github.com>
2019-04-09 20:43:34 +0530

    Update kubernetes-deploy.bat

--- Source/kubernetes-deploy.bat
+++ Source/kubernetes-deploy.bat
@@ -1,4 +1,4 @@
-SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
+SET PATH=%PATH%;"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin"
 SET BUILD_NUMBER=53
 SET IMAGE_NAME=gcr.io/rsdevops/rsmicroimage:%BUILD_NUMBER%
 cd C:\Users\HP\Desktop\PyMicroservices\Source
