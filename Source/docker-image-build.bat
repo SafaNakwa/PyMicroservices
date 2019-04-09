f74182dc94d1a76fff8f92807436cdaf153a02b1
SafaNakwa <49240010+SafaNakwa@users.noreply.github.com>
2019-04-09 20:46:28 +0530

    Update docker-image-build.bat

--- Source/docker-image-build.bat
+++ Source/docker-image-build.bat
@@ -1,4 +1,4 @@
-SET PATH=%PATH%;"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin"
+SET PATH=%PATH%;C:\Users\HP\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
 SET IMAGE_NAME=gcr.io/rsdevops/rsmicroimage:%BUILD_NUMBER%
 cd C:\Users\HP\Desktop\PyMicroservices\Source
 gcloud builds submit --tag %IMAGE_NAME% .
