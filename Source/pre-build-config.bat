c4c8ee09352348f5a65bd3e400060cfb77d5653e
SafaNakwa <49240010+SafaNakwa@users.noreply.github.com>
2019-04-09 20:47:06 +0530

    Update pre-build-config.bat

--- Source/pre-build-config.bat
+++ Source/pre-build-config.bat
@@ -1,2 +1,2 @@
-SET PATH=%PATH%;"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin"
+SET PATH=%PATH%;C:\Users\HP\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
 gcloud auth activate-service-account rsjenkins@rsdevops.iam.gserviceaccount.com --key-file=C:\Users\HP\Downloads\rsdevops-f473d85acfc0.json --project=rsdevops
